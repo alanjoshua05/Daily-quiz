976ff978eac30d2a34250a672d492d530a90d804
alanjoshua05 <155869499+alanjoshua05@users.noreply.github.com>
2024-04-05 15:13:56 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,46 +1,47 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 import streamlit as st
-import pandas as pd
 from datetime import datetime, time
 
-# Function to store data in a DataFrame
-def store_data_in_dataframe(data):
-    if 'df' not in st.session_state:
-        st.session_state.df = pd.DataFrame(columns=['Date', 'Name', 'Email', 'Question 1', 'Question 2'])
-    st.session_state.df = st.session_state.df.append(data, ignore_index=True)
+scopes = ['https://www.googleapis.com/auth/spreadsheets',
+          "https://www.googleapis.com/auth/drive"]
 
-# Function to check if the current time is within the blocked time range
+credentials = ServiceAccountCredentials.from_json_keyfile_name('data-419313-295934c65ef4.json', scopes)
+client = gspread.authorize(credentials)
+
+# Open the desired sheet
+sheet = client.open("FirstSheet").sheet1
+
 def is_blocked_time():
-    blocked_start_time = time(18, 0)  # e.g., 5:00 PM
-    blocked_end_time = time(18, 0)    # e.g., 6:00 PM
+    blocked_start_time = time(17, 0)  # e.g., 5:00 PM
+    blocked_end_time = time(21, 0)    # e.g., 9:00 PM
     now = datetime.now().time()
     return blocked_start_time < now < blocked_end_time
 
-# Streamlit web app
-def main():
-    st.title('Daily quiz')
 
-    # Get input from the user
-    name = st.text_input('Enter Name')
-    email = st.text_input('Enter your college id')
-    st.subheader("Questions")
-    q2 = st.radio("1)hi", ["hello", "hey"], index=None)
-    q1 = st.text_area("2)how are you?")
-    current_date = datetime.now().date()
+    
 
-    if email[7:] != "@drngpit.ac.in" or len(email) > 21:
-        st.error("Enter your college id or else the id shouldn't have empty space at the last")
-    elif is_blocked_time():
-        st.error("Sorry, the quiz is not available at the moment. Please try again later.")
-    else:
-        st.success("Hello ngpian")
+# Streamlit app
+st.title('Daily quiz')
 
-        if st.button('Add Data'):
-            # Create a dictionary with the user input
-            data = {'Date': str(current_date), 'Name': name, 'Email': email, 'Question 1': q2, 'Question 2': q1}
-            store_data_in_dataframe(data)
-            st.success('Data added successfully!')
-            st.write("Current DataFrame:")
-            st.write(st.session_state.df)
+# Get input from the user
+name = st.text_input('Enter Name')
+email = st.text_input('Enter your college id')
 
-if __name__ == '__main__':
-    main()
+st.subheader("Questions")
+q2 = st.radio("1)hi", ["hello", "hey"], index=None)
+q1 = st.text_area("2)how are you?")
+current_date = datetime.now().date()
+
+
+if email[7:] != "@drngpit.ac.in" or len(email) > 21:
+    st.error("Enter your college id or ensure the id doesn't have empty space at the end")
+elif is_blocked_time():
+        st.error("Sorry, the quiz has been timed out.")
+
+else:
+    if st.button("Submit"):
+            # Update the sheet with the entered data
+            sheet.append_row([current_date.strftime('%d-%m-%Y'), name, email, q2, q1])
+            st.success("Added successfully")
+            st.balloons()
